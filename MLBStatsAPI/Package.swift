cb06022420b714e74ace27116972c6698a7fdf3d
Junghoon <junghoon@iBankMacBookPro14.local>
2024-05-21 08:34:54 +0900

    fix

--- MLBStatsAPI/Package.swift
+++ MLBStatsAPI/Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "MLBTeams",
+    name: "MLBStatsAPI",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MLBTeams",
-            targets: ["MLBTeams"]),
+            name: "MLBStatsAPI",
+            targets: ["MLBStatsAPI"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MLBTeams"),
+            name: "MLBStatsAPI"),
         .testTarget(
-            name: "MLBTeamsTests",
-            dependencies: ["MLBTeams"]),
+            name: "MLBStatsAPITests",
+            dependencies: ["MLBStatsAPI"]),
     ]
 )
